351bad3f0b557f95a252ecde27d301eff8f93a56
rickvandervliet <38972062+rickvandervliet@users.noreply.github.com>
2019-07-19 16:12:23 +0200

    Update proportionalRecoveryOverfit.m

--- proportionalRecoveryOverfit.m
+++ proportionalRecoveryOverfit.m
@@ -1,16 +1,16 @@
-load('Data/CombinedTransform.mat')
+load('Data/SimCombinedTransform.mat')
 
-id = repmat(1:size(FMt,2),8,1);
-selectNonNan = ~isnan(FMt);
-FMt = FMt(selectNonNan);
-tt = tt(selectNonNan);
+id = repmat(1:size(dem(1).FMt,2),8,1);
+selectNonNan = ~isnan(dem(1).FMt);
+FMt = dem(1).FMt(selectNonNan);
+tt = dem(1).tt(selectNonNan);
 id = id(selectNonNan);
 NSubjects = numel(unique(id));
 NMeasurements = numel(FMt);
-NChains = 1;
-doparallel = 0;
-NBurnin  = 10^4; 
-NSamples  = 10^4;
+NChains = 10;
+doparallel = 1;
+NBurnin  = 10^3; 
+NSamples  = 10^3;
 gpalpha = 1.6;
 
 %% Overfitted model
@@ -22,6 +22,7 @@ dataStruct = struct('y', FMt, ...
     'NGroups', numGroups,...
     't', tt./7,...
     'gpalpha', gpalpha*ones(numGroups,1),...
+    'bamfordalpha', ones(3,1),...
     'id',id);
 
 init0(1:NChains) = struct('alphaL', cell(1, 1),'rL', cell(1, 1),'gp', cell(1, 1),...
@@ -50,8 +51,28 @@ init0, ...                          % Initial values for latent variables
 'monitorparams', {'yp','g','gpalpha','gp',...
 'tau', 'alpha','r','alpham','alphap'}, ...   % List of latent variables to monitor
 'savejagsoutput' , 1 , ...          % Save command line output produced by JAGS?
-'verbosity' , 0 , ...               % 0=do not produce any output; 1=minimal text output; 2=maximum text output
+'verbosity' , 2 , ...               % 0=do not produce any output; 1=minimal text output; 2=maximum text output
 'cleanup' , 1);                    % clean up of temporary files?
-save(['Output/NumberOfGroupsIs' int2str(numGroups) '.mat'], 'samples', 'stats', 'structArray')
-processProportionalRecoveryModel(samples)
+%save(['Output/NumberOfGroupsIs' int2str(numGroups) '.mat'], 'samples', 'stats', 'structArray')
+
+numClasses = NaN(NChains,1);
+pClasses = NaN(NChains,1);
+for ii=1:NChains
+    [numClasses(ii), pClasses(ii)] = nonEmptyClasses(squeeze(samples.gp(ii,:,:)),0.05);
+end;    
+minClassesInd = find(numClasses==min(numClasses));
+[~,maxPropInd] = max(pClasses(minClassesInd));
+selectInd = minClassesInd(maxPropInd);
+
+samples.yp = samples.yp(selectInd,:);
+samples.g = samples.g(selectInd,:,:);
+samples.gpalpha = samples.gpalpha(selectInd,:,:);
+samples.gp = samples.gp(selectInd,:,:);
+samples.tau = samples.tau(selectInd,:,:);
+samples.alpha = samples.alpha(selectInd,:,:);
+samples.r = samples.r(selectInd,:,:);
+samples.alpham = samples.alpham(selectInd,:,:);
+samples.alphap = samples.alphap(selectInd,:,:);
+
+%proportionalRecoveryProcess(samples,dem(1))
 clear samples stats structArray init0 dataStruct S
