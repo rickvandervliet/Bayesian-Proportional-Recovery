545557426265ef135c8a2faa882785b8e4dd92fb
rickvandervliet <38972062+rickvandervliet@users.noreply.github.com>
2019-07-19 16:10:48 +0200

    Update nonEmptyClasses.m

--- nonEmptyClasses.m
+++ nonEmptyClasses.m
@@ -1,10 +1,9 @@
-function n = nonEmptyClasses(data, cutoff)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-    classes = unique(data);
-    dataMatrix = repmat(data,[1 1 numel(classes)]);
-    classMatrix = permute(repmat(classes,[1,size(data,1),size(data,2)]),[2 3 1]);
-    counts = squeeze((sum(dataMatrix == classMatrix,2)./size(data,2))>cutoff);
-    n=mode(sum(counts,2));
-end
+function [n,p] = nonEmptyClasses(data, cutoff)
+    n = median(sum(data>cutoff,2));
 
+    p = NaN(size(data,1),1);
+    for ii=1:size(data,1)
+        p(ii) = sum(data(ii,data(ii,:)>cutoff));
+    end;
+    p = mean(p);
+end
